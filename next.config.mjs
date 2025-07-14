c30d2a8a8d433c7322dfb4de3b60ad137e26d145
ramin-mammadzada <ramin.mammadzada@vaps.az>
2025-07-14 14:14:06 +0400

    ok

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,14 @@ const nextConfig = {
   images: {
     domains: ["fitnessapp20250501224647.azurewebsites.net"],
   },
+  async rewrites() {
+    return [
+      {
+        source: "/api/:path*",
+        destination: "http://185.233.183.203/api/:path*",
+      },
+    ];
+  },
 };
 
 export default nextConfig;
